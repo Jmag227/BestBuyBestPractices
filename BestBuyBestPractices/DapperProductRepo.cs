fc009a2e5124bb49e83a189b3251e3da22f42d81
Jmag227 <95197785+Jmag227@users.noreply.github.com>
2022-01-04 10:47:38 -0700

    Finished the todos

---
+++ BestBuyBestPractices/DapperProductRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace BestBuyBestPractices
+{
+    internal class DapperProductRepo : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperProductRepo(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+            
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("Insert Into Products " +
+            "(Name, Price, CategoryID) Values (@name, @price, @categoryID);",
+            new { name, price, categoryID });
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _connection.Query<Product>("Select * From Products;");
+        }
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Data;
+using System.IO;
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +11,49 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            var depRepo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Add a new Department");
+            var newDepartment = Console.ReadLine();
+            depRepo.InsertDepartment(newDepartment);
+            var allDepartments = depRepo.GetAllDepartments();
+            foreach (var dep in allDepartments)
+            {
+                Console.WriteLine(dep.Name);
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+            var proRepo = new DapperProductRepo(conn);
+            Console.WriteLine("Add a new Product! We need three things. A name, a price, and a number ID for it!");
+            Console.WriteLine("Input a name for your product!");           
+            string name = Console.ReadLine();
+            Console.WriteLine("Input a price for your Product");
+            double price;
+            double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("Now pick a number between 1-10 for the Category ID");
+            int id;
+            int.TryParse(Console.ReadLine(), out id);
+            proRepo.CreateProduct(name, price, id);
+            Console.WriteLine();
+            var allPro = proRepo.GetAll();
+            foreach (var product in allPro)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine();
+            }
+
+
+
+
         }
     }
 }
